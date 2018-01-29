0196ac92ed4de55aa743fa87237ac8fc61cd4e59
Lucas Rumney <lucasrumney94@gmail.com>
2018-01-29 14:11:34 -0800

    add snakesegment basics

---
+++ trontlet/Assets/SnakeSegment.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SnakeSegment : MonoBehaviour {
+
+	public float gridSize = 1.25f;
+	public float MoveTickTime = 1.0f;
+	public SnakeSegment Leader;
+	public List<SnakeSegment> FollowingSegments;
+	public Vector3 lastPosition;
+	public bool head;
+
+	private GameObject player;
+
+	// Use this for initialization
+	void Start () 
+	{
+		player = GameObject.FindGameObjectWithTag("Player");
+		if (Leader == null)
+		{	
+			Leader = this.gameObject.GetComponent<SnakeSegment>();
+		}
+	}
+	 
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	IEnumerator MoveTowardsPlayer()
+	{
+		lastPosition = transform.position;
+
+		if (Leader == this.gameObject)
+		{
+			// get player location
+			// calculate the move to make
+			// make the move
+
+		}
+		else
+		{
+			// get leader location
+			// move to where the leader was
+			transform.position = Leader.lastPosition;
+
+		}
+		yield return new WaitForSeconds(MoveTickTime);
+	}
+}
